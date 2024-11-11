1b45db31a2ad13a7e8863df2daa83164d95d8bd9
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2024-11-11 14:25:34 +0100

    Update Best_Hyperparameters_for_SVM.py

--- Best_Hyperparameters_for_SVM.py
+++ Best_Hyperparameters_for_SVM.py
@@ -15,16 +15,29 @@ Content : Best  Hyperparameters for SVM
 #========================================
 
 
+
+   
 #========================================
 #--------- L*----------------------------
 #========================================
-
+best_params_={'regressor__model__C': 100,
+ 'regressor__model__epsilon': 0.3,
+ 'regressor__model__gamma': 'scale',
+ 'regressor__model__kernel': 'rbf',
+ 'regressor__scaler': StandardScaler(),
+ 'transformer': StandardScaler()}
 
 
 
 #========================================
 #--------- a*----------------------------
 #========================================
+best_params_={'regressor__model__C': 60,
+ 'regressor__model__epsilon': 0.3,
+ 'regressor__model__gamma': 'scale',
+ 'regressor__model__kernel': 'poly',
+ 'regressor__scaler': None,
+ 'transformer': StandardScaler()}
 
 
 
@@ -33,3 +46,10 @@ Content : Best  Hyperparameters for SVM
 #========================================
 #--------- b*----------------------------
 #========================================
+best_params_={'regressor__model__C': 9,
+ 'regressor__model__epsilon': 0.3,
+ 'regressor__model__gamma': 'scale',
+ 'regressor__model__kernel': 'rbf',
+ 'regressor__scaler': StandardScaler(),
+ 'transformer': StandardScaler()}
+
