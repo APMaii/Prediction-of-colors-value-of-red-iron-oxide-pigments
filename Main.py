f90527ea1151dfdd2f879a3006a061ef98100a29
APMaii <ali.pilehvarmeibody@studenti.polito.it>
2026-02-22 17:05:24 +0100

    new main

--- Main.py
+++ Main.py
@@ -3,8 +3,14 @@ In The name of GOD
 
 Author : Ali Pilehvar Meibody
 
+
 MAIN Workflow
+
+
+
+HERE WE MUST INSERT PREVIOUS HYPERPARAMTER RANGES
 '''
+
 #=====================================
 #=====================================
 #-----IMPORTING libs--------
@@ -13,27 +19,48 @@ MAIN Workflow
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import math
+import random
+import statistics
+import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
+
 from  sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import PowerTransformer
+
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures as pf
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.multioutput import RegressorChain
+from sklearn.compose import TransformedTargetRegressor
 from sklearn.compose import TransformedTargetRegressor as TTR
+from sklearn.preprocessing import FunctionTransformer
+
+
 
 from sklearn.linear_model import LinearRegression as LR
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
 from sklearn.neighbors import KNeighborsRegressor as KNN
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.svm import SVR
 from sklearn.neural_network import MLPRegressor as MLP
 
 
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import  KFold
 
 from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 
@@ -228,13 +255,13 @@ for i in [y1,y2,y3]:
     pipe_model = Pipeline([("scaler", MinMaxScaler()), ("model", model)])
     regressor = TTR(regressor=pipe_model, transformer=StandardScaler())
     fold1=KFold(n_splits=7,shuffle=True,random_state=120)
-    myparams={'regressor__scaler': [None,StandardScaler(),MinMaxScaler(),RobustScaler()],
-          'regressor__model__max_depth': np.arange(1,16),
-          'regressor__model__criterion': ['absolute_error', 'friedman_mse', 'squared_error'],
-          'regressor__model__splitter': ['best','random'],
-          'regressor__model__min_samples_leaf': [0.1,0.3,0.5,0.7,0.9,1,2,3,4,5,6,7,8,9],
-          'regressor__model__min_samples_split': [0.1,0.3,0.5,0.7,0.9,2,3,4,5,6,7,8,9],
-          'transformer': [None,StandardScaler(),MinMaxScaler(),RobustScaler()]}
+    myparams={'regressor__scaler': [None,StandardScaler(),MinMaxScaler()],
+              'regressor__model__max_depth': np.arange(1,6),
+              'regressor__model__criterion': ['absolute_error', 'friedman_mse', 'squared_error'],
+              'regressor__model__splitter': ['best','random'],
+              'regressor__model__min_samples_leaf': [0.1,0.3,0.5,0.7,0.9,1,2,3,4,5,6,7,8,9],
+              'regressor__model__min_samples_split': [0.1,0.3,0.5,0.7,0.9,2,3,4,5,6,7,8,9],
+              'transformer': [None,StandardScaler(),MinMaxScaler()]}
 
     gs = GridSearchCV(regressor, param_grid=myparams, cv=fold1, scoring='neg_mean_absolute_percentage_error',n_jobs=-1)
     gs.fit(x,i)
@@ -290,13 +317,13 @@ for i in [y1,y2,y3]:
     pipe_model = Pipeline([("scaler", MinMaxScaler()), ("model", model)])
     regressor = TTR(regressor=pipe_model, transformer=StandardScaler())
     fold1=KFold(n_splits=7,shuffle=True,random_state=120)
-    myparams={'regressor__scaler': [None,StandardScaler(),MinMaxScaler(),RobustScaler()],
-          'regressor__model__n_estimators': np.arange(1,11),
-          'regressor__model__max_depth': np.arange(1,16),
-          'regressor__model__criterion': ['absolute_error', 'friedman_mse', 'squared_error'],
-          'regressor__model__min_samples_leaf': [0.1,0.3,0.5,0.7,0.9,1,2,3,4,5,6,7,8,9],
-          'regressor__model__min_samples_split': [0.1,0.3,0.5,0.7,0.9,2,3,4,5,6,7,8,9],
-          'transformer': [None,StandardScaler(),MinMaxScaler(),RobustScaler()]}
+    myparams={'regressor__scaler': [None,StandardScaler()],
+              'regressor__model__n_estimators': [10,20,30],
+              'regressor__model__max_depth': np.arange(1,6),
+              'regressor__model__criterion': ['absolute_error', 'friedman_mse', 'squared_error'],
+              'regressor__model__min_samples_leaf': [0.1,0.3,0.5,1,2,3],
+              'regressor__model__min_samples_split': [0.1,0.3,0.5,2,3],
+              'transformer': [None,StandardScaler()]}
     gs = GridSearchCV(regressor, param_grid=myparams, cv=fold1, scoring='neg_mean_absolute_percentage_error',n_jobs=11)
     gs.fit(x,i)
     print('finish grid')
@@ -359,11 +386,11 @@ for i in [y1,y2,y3]:
     fold1=KFold(n_splits=7,shuffle=True,random_state=120)
     
     myparams={'regressor__scaler': [None,StandardScaler(),MinMaxScaler(),RobustScaler()],
-          'regressor__model__kernel': ['linear','poly','rbf','sigmoid'],
-          'regressor__model__C': [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100],
-          'regressor__model__gamma': ['scale','auto'],
-          'regressor__model__epsilon':[0.001,0.01,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9],
-          'transformer': [None,StandardScaler(),MinMaxScaler(),RobustScaler()]}
+              'regressor__model__kernel': ['linear','poly','rbf'],
+              'regressor__model__C': [0.1,1,3,5,7,9,10,20,30,40,50,60,70,80,90,100],
+              'regressor__model__gamma': ['scale','auto'],
+              'regressor__model__epsilon':[0.001,0.01,0.1,0.3,0.5,0.7,0.9],
+              'transformer': [None,StandardScaler(),MinMaxScaler(),RobustScaler()]}
     
     gs = GridSearchCV(regressor, param_grid=myparams, cv=fold1, scoring='neg_mean_absolute_percentage_error',n_jobs=10)
     gs.fit(x,i)
@@ -383,40 +410,33 @@ for i in [y1,y2,y3]:
     print(f'Mean Absolute Percentage Error (MAPE): {mape}')
     print('-'*50)
 
-
 '''
 
-L*
+
 Root Mean Square Error (RMSE): 3.137626606675857
 Mean Absolute Error (MAE): 2.719395210741376
 Coefficient of Determination (R²): 0.5719529486111562
 Mean Absolute Percentage Error (MAPE): 0.12398518024615608
 
 
-
-
-a*
+'''
+'''
 Root Mean Square Error (RMSE): 3.6257783115494973
 Mean Absolute Error (MAE): 3.0036350784539545
 Coefficient of Determination (R²): 0.6080205753004606
 Mean Absolute Percentage Error (MAPE): 0.11020104729093794
 
+'''
 
 
-b*
+
+'''
 Root Mean Square Error (RMSE): 3.4893446751565422
 Mean Absolute Error (MAE): 2.6521487015404617
 Coefficient of Determination (R²): 0.6808212053527699
 Mean Absolute Percentage Error (MAPE): 0.14292891581019923
 
-
-
-
 '''
-
-
-
-
 
 
 
@@ -433,12 +453,12 @@ for i in [y1,y2,y3]:
     
     fold1=KFold(n_splits=7,shuffle=True,random_state=120)
     myparams={'regressor__scaler': [None,Normalizer(),MinMaxScaler(),StandardScaler()],
-          'regressor__model__solver': ['adam','sgd'],
-          'regressor__model__hidden_layer_sizes': [(10,),(100,),(1,1),(2,1),(3,1),(4,1),(5,1),(2,2),(3,2),(4,2),(5,2),(6,2),(3,3),(4,3),(5,3),(6,2),(7,2),(10,5),(10,10),(15,10),(100,100),(3,2,1),(4,3,2),(5,4,3),(6,5,4),(7,6,5),(10,5,1),(15,10,5)],
-          'regressor__model__activation': ['relu','tanh','logistic','identity'],
-          'regressor__model__alpha': [10**-8,10**-7,10**-6,10**-5,10**-4,10**-3,10**-2,0.1,0.2,0.4,0.6,0.8,1],
-          'regressor__model__learning_rate_init': [0.00001,0.0001,0.001,0.01,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1],
-          'transformer': [None,Normalizer(),MinMaxScaler(),StandardScaler()]}
+              'regressor__model__solver': ['adam','sgd'],
+              'regressor__model__hidden_layer_sizes': [(10,),(100,),(1,1),(2,1),(3,1),(4,1),(5,1),(2,2),(3,2),(4,2),(5,2),(6,2),(3,3),(4,3),(5,3),(6,2),(7,2),(10,5),(10,10),(15,10),(100,100),(3,2,1),(4,3,2),(5,4,3),(6,5,4),(7,6,5),(10,5,1),(15,10,5)],
+              'regressor__model__activation': ['relu','tanh','logistic','identity'],
+              'regressor__model__alpha': [10**-8,10**-6,10**-4,10**-2,0.1,0.2,0.4,0.6,0.8,1],
+              'regressor__model__learning_rate_init': [0.00001,0.0001,0.001,0.01,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1],
+              'transformer': [None,Normalizer(),MinMaxScaler(),StandardScaler()]}
     
     gs = GridSearchCV(regressor, param_grid=myparams, cv=fold1, scoring='neg_mean_absolute_percentage_error',n_jobs=11)
     gs.fit(x,i)
@@ -460,28 +480,49 @@ for i in [y1,y2,y3]:
 
 
 
-'''
 
-L*
+'''
 Root Mean Square Error (RMSE): 4.05968665042347
 Mean Absolute Error (MAE): 2.6808407806275865
 Coefficient of Determination (R²): 0.28340459557334385
 Mean Absolute Percentage Error (MAPE): 0.1136309703675417
 
+'''
 
 
-a*
+
+
+'''
 Root Mean Square Error (RMSE): 4.3551523070908535
 Mean Absolute Error (MAE): 3.5846925728121155
 Coefficient of Determination (R²): 0.4344546019342884
 Mean Absolute Percentage Error (MAPE): 0.12947594266607682
 
+'''
 
-b*
+
+
+
+
+'''
 Root Mean Square Error (RMSE): 3.9103676475306535
 Mean Absolute Error (MAE): 2.981299551105248
 Coefficient of Determination (R²): 0.5991503850625958
 Mean Absolute Percentage Error (MAPE): 0.16853414744530612
+
 '''
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
